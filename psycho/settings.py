87df58492448aae1da7990e5353e5e7fd4adbb2a
wbubicz <wojciech.bubicz@gmail.com>
2016-03-02 20:27:16 +0100

    Dodany ekran logowania

--- psycho/settings.py
+++ psycho/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
+	'arche',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -118,3 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = (
+# 	os.path.join(BASE_DIR, "static"),
+# )
+#
+# STATIC_ROOT = os.path.join('/', 'static')
+#
+# LOGIN_REDIRECT_URL = '/'
+
+LOGIN_REDIRECT_URL = '/'
